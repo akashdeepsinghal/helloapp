57659377c114d5e4fe36c2b4fda70f6887495fce
Jonah Marsden <jonah@podpress.io>
2023-07-28 13:07:58 +0100

    test

---
+++ apps/frontend/src/app/label/label.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable-next-line */
+export interface LabelProps {}
+
+import { useEffect, useState } from 'react';
+
+export function Label(props: LabelProps) {
+    const [resp, setResp] = useState<any>([]);
+
+    useEffect(() => {
+        fetch('/api')
+            .then((_) => _.json())
+            .then(setResp);
+    }, []);
+
+    return (
+        <div>
+            <h1>Welcome to Label! With: { resp.message }</h1>
+        </div>
+    );
+    
+}
+
+export default Label;
