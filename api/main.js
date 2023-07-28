8cad3d6ff0133f01a5e53dac84f39a1f3425e83f
Jonah Marsden <jonah@podpress.io>
2023-07-28 17:04:48 +0100

    123

---
+++ api/main.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+// Import and set up your actual Express.js app from the original location
+const actualApp = require('./apps/backend/main.js');
+actualApp(app);
+
+// Export the serverless function
+module.exports = (req, res) => {
+  app(req, res);
+};
