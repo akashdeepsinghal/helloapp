f302acd45354d521cc437282e462d07fb90da72d
Jonah Marsden <jonah@podpress.io>
2023-07-28 17:21:42 +0100

    1222

--- api/main.js
+++ api/main.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const app = express();
 
 import { core } from '@backend/core';
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+const { v4 } = require('uuid');
 
 app.get('/api', (req, res) => {
-  res.send({ message: core() });
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-// Export the serverless function
-module.exports = (req, res) => {
-  app(req, res);
-};
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = app;
